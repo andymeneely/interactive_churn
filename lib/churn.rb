5fd0204a7c61b685fa0265d92c09856cb405ecb7
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-09 13:26:16 -0500

    add Churn::root_directory attr_accessor, and Churn::compute method, compute method should raise an exception if root_directory is not a directory

--- lib/churn.rb
+++ lib/churn.rb
@@ -1,3 +1,12 @@
 class Churn
   COMMAND_NAME = "ichurn"
+
+  class << self
+    attr_accessor :root_directory
+  end
+
+  def self.compute
+    raise StandardError, "#{Churn::COMMAND_NAME}: #{Churn.root_directory}: No such file or directory" unless Dir.exists?(root_directory)
+  end
+
 end
--- spec/churn_spec.rb
+++ spec/churn_spec.rb
@@ -4,4 +4,10 @@ describe "Churn" do
   it "has a constant that stores the command name" do
     expect(Churn::COMMAND_NAME).to match(/^ichurn$/)
   end
+
+  it "raises an exception if root_directory is not a directory" do
+    directory_name = "not_a_directory"
+    Churn.root_directory = directory_name
+    expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{directory_name}: No such file or directory")
+  end
 end
