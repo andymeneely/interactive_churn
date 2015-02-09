21db025bd8bb8566e82d67635c12f647eda329db
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-09 14:22:23 -0500

    refactor test examples, add before and after each block

--- spec/churn_spec.rb
+++ spec/churn_spec.rb
@@ -11,37 +11,35 @@ describe "Churn" do
     expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{directory_name}: No such file or directory")
   end
 
-  it "has the same current directory before and after call compute" do
-    cwd = Dir.getwd
-    directory_name = Dir.getwd + "/.." + "/churn_test_directory"
-    system("mkdir #{directory_name}")
+  context "within an existing directory" do
+    before(:each) do
+      @directory_name = Dir.getwd + "/.." + "/churn_test_directory"
+      system("mkdir #{@directory_name}")
+      Churn.root_directory = @directory_name
+    end
 
-    Churn.root_directory = directory_name
-    Churn.compute rescue # => no matter if there is an exception, the expectation should be met
-    expect(cwd).to eq(Dir.getwd)
+    it "has the same current directory before and after call compute" do
+      cwd = Dir.getwd
+      Churn.root_directory = @directory_name
+      Churn.compute rescue # => no matter if there is an exception, the expectation should be met
+      expect(cwd).to eq(Dir.getwd)
+    end
 
-    system("rm -r -f #{directory_name}")
-  end
+    it "raises an exception if the root_directory is not a git repository" do
+      Churn.root_directory = @directory_name
+      expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{@directory_name}: fatal: Not a git repository (or any of the parent directories): .git")
+    end
 
-  it "raises an exception if the root_directory is not a git repository" do
-    directory_name = Dir.getwd + "/.." + "/churn_test_directory"
-    system("mkdir #{directory_name}")
+    it "raises an exception if the root_directory is a git repository with no commits" do
+      system("git init #{@directory_name}")
 
-    Churn.root_directory = directory_name
-    expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{directory_name}: fatal: Not a git repository (or any of the parent directories): .git")
+      Churn.root_directory = @directory_name
+      expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{@directory_name}: fatal: bad default revision 'HEAD'")
+    end
 
-    system("rm -r -f #{directory_name}")
-  end
-
-  it "raises an exception if the root_directory is a git repository with no commits" do
-    directory_name = Dir.getwd + "/.." + "/churn_test_directory"
-    system("mkdir #{directory_name}")
-    system("git init #{directory_name}")
-
-    Churn.root_directory = directory_name
-    expect { Churn.compute }.to raise_error(StandardError, "ichurn: #{directory_name}: fatal: bad default revision 'HEAD'")
-
-    system("rm -r -f #{directory_name}")
+    after(:each) do
+      system("rm -r -f #{@directory_name}")
+    end
   end
 
 end
