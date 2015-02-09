6609fbab01eda8175f4936290e54a79bb5f0e689
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-09 15:13:45 -0500

    refactor code, use begin-rescue block to compute churn

--- lib/churn.rb
+++ lib/churn.rb
@@ -7,24 +7,28 @@ class Churn
 
   def self.compute revision=""
     cwd = Dir.getwd
-
-    raise StandardError, "#{Churn::COMMAND_NAME}: #{Churn.root_directory}: No such file or directory" unless Dir.exists?(root_directory)
-
-    Dir.chdir root_directory
-    output = %x[ git rev-parse --is-inside-work-tree #{root_directory} 2>&1 ].tr("\n","")
-    Dir.chdir cwd
-    raise StandardError, "ichurn: #{root_directory}: " + output unless output =~ /^true/
-
-    Dir.chdir root_directory
-    output = %x[ git log -p -1 2>&1 ].tr("\n","")
-    Dir.chdir cwd
-    raise StandardError, "ichurn: #{root_directory}: " + output unless output =~ /^commit/
-
-    Dir.chdir root_directory
-    output = %x[ git log -p -1 #{revision} 2>&1 ].split(/\n/)[0]
-    Dir.chdir cwd
-    raise StandardError, "ichurn: " + output unless output =~ /^commit/
-
+    begin
+      Dir.chdir root_directory
+      check_exceptions revision
+    rescue Errno::ENOENT
+      raise StandardError, "#{Churn::COMMAND_NAME}: #{Churn.root_directory}: No such file or directory"
+    rescue StandardError => e
+      raise e
+    ensure
+      Dir.chdir cwd
+    end
   end
+
+  private
+    def self.check_exceptions revision
+      output = %x[ git rev-parse --is-inside-work-tree #{root_directory} 2>&1 ].tr("\n","")
+      raise StandardError, "ichurn: #{root_directory}: " + output unless output =~ /^true/
+
+      output = %x[ git log -p -1 2>&1 ].tr("\n","")
+      raise StandardError, "ichurn: #{root_directory}: " + output unless output =~ /^commit/
+
+      output = %x[ git log -p -1 #{revision} 2>&1 ].split(/\n/)[0]
+      raise StandardError, "ichurn: " + output unless output =~ /^commit/
+    end
 
 end
