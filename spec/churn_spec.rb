50d5a670665fb1af5f74f8e7c623bd962e4e6215
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-27 19:32:49 -0500

    using samplerepo git submodule to test

--- spec/churn_spec.rb
+++ spec/churn_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Churn" do
+describe "Churn class" do
   COMMAND_NAME = 'ichurn'
 
   it "has a constant that stores the command name" do
@@ -17,7 +17,7 @@ describe "Churn" do
     expect(Churn.count_lines_from [" 1 file changed", " 2 insertions(+)", " 1 deletion(-)", " 1 file changed", " 1 insertion(+)"]).to include(insertions: 3, deletions: 1)
   end
 
-  context "within an existing directory" do
+  context "within a plain directory (non-git repo)" do
     before(:each) do
       @directory_name = Dir.getwd + "/.." + "/churn_test_directory"
       system("mkdir #{@directory_name}")
@@ -34,88 +34,65 @@ describe "Churn" do
       expect { Churn.compute }.to raise_error(StandardError, "#{COMMAND_NAME}: #{@directory_name}: fatal: Not a git repository (or any of the parent directories): .git")
     end
 
-    context "within a git repository" do
-      before(:each) do
-        system("git init #{@directory_name}")
-        system("cd #{@directory_name} && git config user.email 'atester@ichurn.org' && git config user.name 'auto tester'")
-      end
+    after(:each) do
+      system("rm -r -f #{@directory_name}")
+    end
+  end
 
-      it "raises an exception if the root_directory is a git repository with no commits" do
-        expect { Churn.compute }.to raise_error(StandardError, "#{COMMAND_NAME}: #{@directory_name}: fatal: bad default revision 'HEAD'")
-      end
+  context "within a git repo with no commits" do
+    before(:each) do
+      @directory_name = Dir.getwd + "/.." + "/churn_test_directory"
+      system("git init #{@directory_name}")
+      Churn.root_directory = @directory_name
+    end
 
-      it "raises an exception if try to compute churn for an unknown revision" do
-        file_name = "a_file"
-        system("cd #{@directory_name} && touch #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-        revision = "UNKNOWN_REVISION"
-
-        expect { Churn.compute revision: revision }.to raise_error(StandardError, "#{COMMAND_NAME}: fatal: ambiguous argument '#{revision}': unknown revision or path not in the working tree.")
-      end
-
-      it "has HEAD as default revision" do
-        file_name = "a_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")        
-        expect(Churn.compute).to eq(Churn.compute revision: "HEAD")
-      end
-
-      it "computes the amount of inserted lines on the current branch for the entire history, all files, and default HEAD revision" do
-        file_name = "a_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-        expect(Churn.compute[:insertions]).to eq(1)
-        system("cd #{@directory_name} && echo 'line1\nline2\nline3' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-        expect(Churn.compute[:insertions]).to eq(3)
-
-        file_name = "another_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-        expect(Churn.compute[:insertions]).to eq(4)
-        system("cd #{@directory_name} && echo 'line1\nline2' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-        expect(Churn.compute[:insertions]).to eq(5)
-      end
-
-      it "computes the amount of deleted lines on the current branch for the entire history, all files, and default HEAD revision" do
-          file_name = "a_file"
-          system("cd #{@directory_name} && echo 'line1' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-          expect(Churn.compute[:deletions]).to eq(0)
-          system("cd #{@directory_name} && echo 'line2\nline3' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-          expect(Churn.compute[:deletions]).to eq(1)
-
-          file_name = "another_file"
-          system("cd #{@directory_name} && echo 'line1\nline2' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-          expect(Churn.compute[:deletions]).to eq(1)
-          system("cd #{@directory_name} && echo 'line3' > #{file_name} && git add #{file_name} && git commit -m 'initial commit'")
-          expect(Churn.compute[:deletions]).to eq(3)
-      end
-
-      it "raises an exception if the file does not exist" do
-        file_name = "non-existent-file"
-        system("cd #{@directory_name} && touch a_file && git add a_file && git commit -m 'initial commit'")
-        expect { Churn.compute file_name: file_name}.to raise_error(StandardError, "#{COMMAND_NAME}: fatal: ambiguous argument '#{file_name}': unknown revision or path not in the working tree.")
-      end
-
-      it "computes the amount of inserted lines on a specific file on the current branch for the entire" do
-        file_a = "a_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_a} && git add #{file_a} && git commit -m 'initial commit'")
-        system("cd #{@directory_name} && echo 'line1\nline2\nline3' > #{file_a} && git add #{file_a} && git commit -m 'initial commit'")
-
-        file_b = "another_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_b} && git add #{file_b} && git commit -m 'initial commit'")
-        system("cd #{@directory_name} && echo 'line1\nline2' > #{file_b} && git add #{file_b} && git commit -m 'initial commit'")
-
-        expect(Churn.compute(file_name: file_a)[:insertions]).to eq(3)
-        expect(Churn.compute(file_name: file_b)[:insertions] ).to eq(2)
-      end
-
-      it "returns a summary of the history" do
-        file_a = "a_file"
-        system("cd #{@directory_name} && echo 'line1' > #{file_a} && git add #{file_a} && git commit -m 'initial commit'")
-        system("cd #{@directory_name} && echo 'line2\nline3' > #{file_a} && git add #{file_a} && git commit -m 'initial commit'")
-        expect(Churn.git_history_summary).to eq([" 1 file changed", " 2 insertions(+)", " 1 deletion(-)", " 1 file changed", " 1 insertion(+)"])
-      end
+    it "raises an exception if the root_directory is a git repository with no commits" do
+      expect { Churn.compute }.to raise_error(StandardError, "#{COMMAND_NAME}: #{@directory_name}: fatal: bad default revision 'HEAD'")
     end
 
     after(:each) do
       system("rm -r -f #{@directory_name}")
     end
+  end
+
+  context "within a git repository" do
+    before(:each) do
+      @directory_name = Dir.getwd + "/spec/samplerepo"
+      Churn.root_directory = @directory_name
+    end
+
+    it "raises an exception if try to compute churn for an unknown revision" do
+      revision = "UNKNOWN_REVISION"
+      expect { Churn.compute revision: revision }.to raise_error(StandardError, "#{COMMAND_NAME}: fatal: ambiguous argument '#{revision}': unknown revision or path not in the working tree.")
+    end
+
+    it "has HEAD as default revision" do
+      expect(Churn.compute).to eq(Churn.compute revision: "HEAD")
+    end
+
+    it "computes the amount of inserted lines on the current branch for the entire history, all files, and default HEAD revision" do
+      expect(Churn.compute[:insertions]).to eq(48)
+    end
+
+    it "computes the amount of deleted lines on the current branch for the entire history, all files, and default HEAD revision" do
+        expect(Churn.compute[:deletions]).to eq(20)
+    end
+
+    it "raises an exception if the file does not exist" do
+      file_name = "non-existent-file"
+      expect { Churn.compute file_name: file_name}.to raise_error(StandardError, "#{COMMAND_NAME}: fatal: ambiguous argument '#{file_name}': unknown revision or path not in the working tree.")
+    end
+
+    it "computes the amount of inserted lines on a specific file on the current branch for the entire" do
+      expect(Churn.compute(file_name: "factorial.rb")[:insertions]).to eq(35)
+      expect(Churn.compute(file_name: "test.rb")[:insertions] ).to eq(12)
+    end
+
+    it "returns a summary of the history" do
+      file_a = "test.rb"
+      expect(Churn.git_history_summary).to eq([" 2 files changed", " 8 insertions(+)", " 2 deletions(-)", " 2 files changed", " 6 insertions(+)", " 1 deletion(-)", " 2 files changed", " 7 insertions(+)", " 2 files changed", " 5 insertions(+)", " 3 deletions(-)", " 2 files changed", " 4 insertions(+)", " 3 deletions(-)", " 1 file changed", " 1 insertion(+)", " 1 file changed", " 2 insertions(+)", " 8 deletions(-)", " 1 file changed", " 7 insertions(+)", " 3 deletions(-)", " 1 file changed", " 7 insertions(+)", " 1 file changed", " 1 insertion(+)"])
+    end
+
   end
 
 end
