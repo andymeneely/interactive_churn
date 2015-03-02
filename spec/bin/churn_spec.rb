c3488fe1453936685be6bec9eb762b2861f841d2
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-03-02 09:28:19 -0500

    Change directory when testing default `churn` command

--- spec/bin/churn_spec.rb
+++ spec/bin/churn_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Churn command" do
   it "prints churn metrics as text by default" do
-    output = %x[ cd spec/samplerepo & churn ]
+    output = %x[ churn ]
     expect(output).to match /^Commits:\s*\d*\sTotal Churn:\s*\d*\sLines added:\s*\d*\sLines deleted:\s*\d*\s$/
   end
   it "prints churn metrics as json" do
