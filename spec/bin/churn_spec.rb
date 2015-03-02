5c3bb1d308481c2545db267a228dbc5fa1701b35
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-03-02 09:26:51 -0500

    Churn command accepts --json option

--- spec/bin/churn_spec.rb
+++ spec/bin/churn_spec.rb
@@ -5,5 +5,9 @@ describe "Churn command" do
     output = %x[ cd spec/samplerepo & churn ]
     expect(output).to match /^Commits:\s*\d*\sTotal Churn:\s*\d*\sLines added:\s*\d*\sLines deleted:\s*\d*\s$/
   end
+  it "prints churn metrics as json" do
+    output = %x[ churn --json]
+    expect(output).to match /^{\"Commits\":\d*,\"Total Churn\":\d*,\"Lines added\":\d*,\"Lines deleted\":\d*}\n$/
+  end
 end
 
