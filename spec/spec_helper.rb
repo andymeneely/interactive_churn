65f87ab83dabd0688e5f5625b77005be6d9278d0
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-03-18 21:12:04 -0400

    Add coveralls gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+require 'simplecov'
+require 'coveralls'
+
 require_relative '../lib/churn'
 require_relative '../lib/churn_affected_line'
 require_relative '../lib/churn_interactive'
 require_relative '../lib/churn_standard'
 require_relative '../lib/text_output'
 require_relative '../lib/json_output'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
