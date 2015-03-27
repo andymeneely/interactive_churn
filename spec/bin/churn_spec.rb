05dc1122c294dfe06d7b2d890fd2ea7256f53778
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-03-27 13:23:02 -0400

    Use ChurnCLI class in churn script

--- spec/bin/churn_spec.rb
+++ spec/bin/churn_spec.rb
@@ -3,29 +3,28 @@ require 'spec_helper'
 describe "Churn command" do
   it "prints churn metrics as text by default" do
     output = %x[ churn ]
-    # expect(output).to match /^Commits:\s*\d*\sTotal Churn:\s*\d*\sLines added:\s*\d*\sLines deleted:\s*\d*\s$/
-    expect(output).to match /^Standard churn:\s*\d*\s$/
+    expect(output).to match /^Commits:\s*\d*\nStandard churn:\s*\d*\nDeletions:\s*\d*\nInsertions:\s*\d*\s$/
   end
   it "prints churn metrics as json" do
     output = %x[ churn --json]
-    # expect(output).to match /^{\"Commits\":\d*,\"Total Churn\":\d*,\"Lines added\":\d*,\"Lines deleted\":\d*}\n$/
-    expect(output).to match /^{\"Standard churn\":\d*}\n$/
+    expect(output).to match /^{\"Commits:\":\d+,\"Standard churn:\":\d+,\"Deletions:\":\d+,\"Insertions:\":\d+}\n/
   end
   it "prints affected lines as text by default" do
     output = %x[ churn --affected-lines ]
-    expect(output).to match /^Affected lines:\s*\d*\s$/
+    expect(output).to match /^Commits:\s+\d+\nAffected lines churn:\s+\d+\n$/
   end
   it "prints affected lines as json" do
     output = %x[ churn --affected-lines --json]
-    expect(output).to match /^{\"Affected lines\":\d*}\n$/
+    expect(output).to match /^{\"Commits:\":\d+,\"Affected lines churn:\":\d+}\n$/
+    # expect(output).to match /^{\"Affected lines\":\d*}\n$/
   end
   it "prints interactive lines as text by default" do
     output = %x[ churn --interactive-lines ]
-    expect(output).to match /^Interactive lines:\s*\d*\s$/
+    expect(output).to match /Commits:\s+\d+\nInteractive churn:\s+\d+\nSefl churn:\s+\d+\nAuthors affected:\s+\d+\n/
   end
   it "prints interactive lines as json" do
     output = %x[ churn --interactive-lines --json]
-    expect(output).to match /^{\"Interactive lines\":\d*}\n$/
+    expect(output).to match /^{\"Commits:\":\d+,\"Interactive churn:\":\d+,\"Sefl churn:\":\d+,\"Authors affected:\":\d+}\n$/
   end
   it "returns an error when --interactive-lines --affected-lines are passed" do
     msg = "parse error: --affected-lines and --interactive-lines are mutually exclusive\n" +
